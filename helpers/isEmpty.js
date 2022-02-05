a8f2f2e0fdecb7299d48445498eb7e6f31bca9a5
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-02-05 23:09:27 +0300

    feat: helper isEmptyArr

--- helpers/isEmpty.js
+++ helpers/isEmpty.js
@@ -1,3 +1,7 @@
 export function isEmptyObj(value) {
   return value && Object.keys(value).length === 0
 }
+
+export function isEmptyArr(value) {
+  return value && Array.isArray(value) && !value.length
+}
