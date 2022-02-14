aa152dbf4f245b18bd416bfb5e1cca7dae25a084
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-02-14 16:04:19 +0300

    refactor: base paginator script

--- components/PaginatorList.vue
+++ components/PaginatorList.vue
@@ -5,17 +5,5 @@
 </template>
 
 <script>
-export default {
-  mounted() {
-    this.fetchFeedListCount()
-  },
-
-  methods: {
-    async fetchFeedListCount() {
-      const res = await fetch("http://localhost:3005/feedCountByTag")
-      const data = await res.json()
-      console.log(data)
-    },
-  },
-}
+export default {}
 </script>
