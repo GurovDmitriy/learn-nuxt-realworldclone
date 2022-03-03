754c96caf1d3c30ccf41e507fc0d085d39f2323d
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2022-03-03 17:10:33 +0300

    feat: feedblock

--- components/FeedBlock.vue
+++ components/FeedBlock.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <PlaceholderBlock />
+  <div class="feed">
+    <p class="feed__content">{{ dataItem.content }}</p>
+    <TagsList
+      :data-item="dataItem.tags"
+      class="feed-list-item-footer__tags-list"
+    />
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    dataItem: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.feed {
+  padding: $space-l 0;
+
+  border-bottom: 2px solid $var-color-grey-light;
+}
+
+.feed__preview {
+  margin-bottom: $space-m;
+}
+</style>
